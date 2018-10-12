926e0928918153efe8830a620411400a1f4c6f3f
Kendra <kendra@coffeeringsdesign.com>
2018-10-12 13:28:04 -0700

    begin to flesh out if statements for each requirement

--- src/main.js
+++ src/main.js
@@ -23,13 +23,44 @@ $(document).ready(function() {
       if (body.length === 0) {
         $("#errorDisplay").text("We are very sorry but there are no Doctors that meets that criteria");
       } else {
-        for(let i = )
+        for(let i = 0, i <= body.data.length; i++){
+
+          let city = body.data[i].practices[0].visit_address.city;
+          let state = body.data[i].practices[0].visit_address.state;
+          let zip = body.data[i].practices[0].visit_address.zip;
+
+          if (body.data[i].practices[0].visit_address.street === undefined) {
+            let street = "";
+          } else {
+            let street = body.data[i].practices[0].visit_address.street;
+          }
+
+          if (body.data[i].practices[0].visit_address.street2 === undefined) {
+            let street2 = "";
+          } else {
+            let street2 = body.data[i].practices[0].visit_address.street2;
+          }
+
+          if (body.data[i].practices[0].visit_address.street2 === undefined) {
+            let street2 = "";
+          } else {
+            let street2 = body.data[i].practices[0].visit_address.street2;
+          }
+
+          if (body.data[i].practices[0].accepts_new_patients === true) {
+            let acceptsNewPatients = "Yes";
+          } else if (body.data[i].practices[0].accepts_new_patients === false) {
+            let acceptsNewPatients = "No";
+          } else {
+            let acceptsNewPatients = "Unknown";
+          }
+        }
       }
       $("#doctorNameDisplay").text(`Dr. ${body.data[0].profile.first_name} ${body.data[0].profile.last_name}
       Accepting New Patients: ${body.data[0].practices[0].accepts_new_patients}
-      Address: ${body.data[0].practices[0].visit_address.street},  ${body.data[0].practices[0].visit_address.street2}, ${body.data[0].practices[0].visit_address.city}, ${body.data[0].practices[0].visit_address.state}, ${body.data[0].practices[0].visit_address.zip}
+
+
       Phone: ${body.data[0].practices[0].phones[0].number},
-      ${body.data[0].practices[0].phones[0].type},
       Website: ${body.data[0].practices[0].website}
       Biography: ${body.data[0].profile.bio}`);
 
