6f29845630da74b6f238faf69eb8e66e6ebe7488
Kendra <kendra@coffeeringsdesign.com>
2018-10-12 13:43:07 -0700

    get all if/else statements to log correctly

--- src/main.js
+++ src/main.js
@@ -23,51 +23,59 @@ $(document).ready(function() {
       if (body.length === 0) {
         $("#errorDisplay").text("We are very sorry but there are no Doctors that meets that criteria");
       } else {
-        for(let i = 0, i <= body.data.length; i++){
+        for(let i=0; i <= body.data.length; i++){
 
+          let doctorName = `"Dr. ${body.data[i].profile.first_name} ${body.data[i].profile.last_name}`;
           let city = body.data[i].practices[0].visit_address.city;
           let state = body.data[i].practices[0].visit_address.state;
           let zip = body.data[i].practices[0].visit_address.zip;
+          let street;
+          let street2;
+          let phone;
+          let acceptsNewPatients;
+          let website;
+          let biography;
 
           if (body.data[i].practices[0].visit_address.street === undefined) {
-            let street = "";
+            street = "";
           } else {
-            let street = body.data[i].practices[0].visit_address.street;
+            street = body.data[i].practices[0].visit_address.street;
           }
 
           if (body.data[i].practices[0].visit_address.street2 === undefined) {
-            let street2 = "";
+            street2 = "";
           } else {
-            let street2 = body.data[i].practices[0].visit_address.street2;
-          }
-
-          if (body.data[i].practices[0].visit_address.street2 === undefined) {
-            let street2 = "";
-          } else {
-            let street2 = body.data[i].practices[0].visit_address.street2;
+            street2 = body.data[i].practices[0].visit_address.street2;
           }
 
           if (body.data[i].practices[0].accepts_new_patients === true) {
-            let acceptsNewPatients = "Yes";
+            acceptsNewPatients = "Yes";
           } else if (body.data[i].practices[0].accepts_new_patients === false) {
-            let acceptsNewPatients = "No";
+            acceptsNewPatients = "No";
           } else {
-            let acceptsNewPatients = "Unknown";
+            acceptsNewPatients = "Unknown";
+          }
+
+          if (body.data[i].practices[0].phones[0].number === undefined) {
+            phone = "currently unavailable";
+          } else {
+            phone = body.data[i].practices[0].phones[0].number;
+          }
+
+          if (body.data[i].practices[0].website === undefined) {
+            website = "currently unavailable";
+          } else {
+            website = body.data[i].practices[0].website;
+          }
+
+          if (body.data[i].profile.bio === undefined) {
+            biography = "currently unavailable";
+          } else {
+            biography = body.data[i].profile.bio;
+            console.log(biography, website, phone, acceptsNewPatients, street, street2, city, state, zip, doctorName);
           }
         }
       }
-      $("#doctorNameDisplay").text(`Dr. ${body.data[0].profile.first_name} ${body.data[0].profile.last_name}
-      Accepting New Patients: ${body.data[0].practices[0].accepts_new_patients}
-
-
-      Phone: ${body.data[0].practices[0].phones[0].number},
-      Website: ${body.data[0].practices[0].website}
-      Biography: ${body.data[0].profile.bio}`);
-
-
-
-
-
 
     }, function(error) {
       $(".errorDisplay").text(`There was an error processing your request: ${error.message}`);
